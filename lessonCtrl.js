e1728b015ee7db5a004072bd46f441744a17ebfd
Joseph Blank <josephblank@Josephs-MacBook-Pro.local>
2016-10-04 16:54:17 -0600

    created custom directive

---
+++ lessonCtrl.js
@@ -0,0 +1,16 @@
+angular.module('directivePractice')
+    .controller('lessonCtrl', function($scope, lessonService) {
+
+        $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+        $scope.test = 'Two-way data binding';
+        $scope.announceDay = function(lesson, day) {
+          if (day) {
+          alert(lesson + ' is active on ' + day + '.');
+        } else {
+          alert(lesson + ' is active this week');
+        }
+        }
+
+
+
+    })
---
+++ lessonHider.js
@@ -0,0 +1,47 @@
+angular.module('directivePractice')
+    .directive('lessonHider', function() {
+
+        return {
+          restrict: 'E',
+          templateUrl: 'lessonHider.html',
+          scope: {
+            lesson: '=',
+            dayAlert: '&',
+          },
+
+          controller: function($scope, lessonService) {
+              $scope.getSchedule = lessonService.getSchedule();
+          },
+
+          link: function(scope, element, attributes) {
+            scope.getSchedule.then(function(response) {
+              scope.schedule = response.data;
+
+
+              for (var i = 0; i < scope.schedule.length; i++) {
+                if (scope.schedule[i].lesson === scope.lesson) {
+                  element.css('text-decoration', 'line-through');
+                  scope.lessonDay = scope.schedule[i].weekday;
+                  return;
+                }
+              }
+              
+
+              // scope.schedule.forEach(function(scheduleDay) {
+              //   if (scheduleDay.lesson === scope.lesson) {
+              //     element.css('text-decoration', 'line-through');
+              //     scope.lessonDay = scheduleDay.weekday;
+              //     return;
+              //   }
+              // })
+
+            })
+
+
+
+          }
+        }
+
+
+
+    })
